4436a13d0b3f469589e772dc7b18427b06f9bbc6
Aldo Perri <130299266+perri99@users.noreply.github.com>
2024-08-29 16:12:43 +0200

    commenti, rifiuto primo già inserito, sorting

--- 29-08/esercizio_numeri_primi.py
+++ 29-08/esercizio_numeri_primi.py
@@ -1,15 +1,20 @@
-prime_numbers = []
+prime_numbers = [] #inizializzazione lista di numeri primi
 
-while len(prime_numbers) < 5:
-    prime = True
+while len(prime_numbers) < 5: #continuo il ciclo finchè non ho 5 elementi in prime_numbers
+    prime = True #variabile di controllo di primalità
     number = int(input('Inserisci un numero intero: '))
-    for div in range(2, number): #controllo se il numero è primo, check sui possibili divisori
-        if number % div == 0:
-            prime = False
-    if prime == True:
-        prime_numbers.append(number)
-        print('Hai inserito un numero primo: ', number)
+    if number in prime_numbers: #se è un primo già inserito non lo accetto
+        print('Hai già inserito questo numero primo')
     else:
-        print('Il numero inserito non è primo!')
+        for div in range(2, number): #controllo se il numero è primo, check sui possibili divisori, si potrebbe diminuire il numero di iterazioni
+            if number % div == 0:
+                prime = False #se ottengo una divisione esatta allora number non è primo
+        if prime == True:
+            prime_numbers.append(number) #aggiungo il numero primo alla lista
+            print('Hai inserito un numero primo: ', number)
+        else:
+            print('Il numero inserito non è primo!')
+
+prime_numbers.sort()
 print('Hai inserito 5 numeri primi:')
 print(prime_numbers)
