82aadd8c40b7770f058237030a2181195cf9a347
Aldo Perri <130299266+perri99@users.noreply.github.com>
2024-08-29 17:12:10 +0200

    Commenti

--- 29-08/esercizio_finale2.py
+++ 29-08/esercizio_finale2.py
@@ -1,8 +1,8 @@
-exit = False
+exit = False #variabile controllo while
 
-while exit != True:
+while exit != True: #se exit è False continua a iterare
     number = int(input('Inserisci un numero: '))
-    numbers_list = []
+    numbers_list = [] #lista vuota da riempire
     for num in range(number+1):
         numbers_list.append(number-num)
     print(numbers_list)
