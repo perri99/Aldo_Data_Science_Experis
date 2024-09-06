5e7044da7aa5c9b40b894816de4a6f518c586015
Aldo Perri <130299266+perri99@users.noreply.github.com>
2024-09-06 11:16:43 +0200

    bug fixed

--- 06-09/09/fibonacci.py
+++ 06-09/09/fibonacci.py
@@ -1,19 +1,21 @@
 '''
 Fibonacci
 '''
-def fibonacci_number(number): #https://www.valcon.it/python/numeri-di-fibonacci/
-    if number <= 2:
+def fibonacci_number(number): #https://www.valcon.it/python/numeri-di-fibonacci/ leggera modifica
+    if number == 0:
+        return 0
+    elif number <=2:
         return 1
     else:
-        return fibonacci_number(number-1) + fibonacci_number(number-2) #restituisce n-esimo numero di fibonacci
+        return fibonacci_number(number-1) + fibonacci_number(number-2)
 
 def find_fibonacci(number):
     fibonacci_numbers = []
-    for index in range(number):
+    for index in range(number+1):
         fibonacci = fibonacci_number(index)
-        if fibonacci < number:
+        if fibonacci <= number:
             fibonacci_numbers.append(fibonacci_number(index))
-    return fibonacci_numbers #restituisce una lista dei numeri di fibonacci minori di n
+    return fibonacci_numbers
 
 input_number = int(input('Inserisce un numero: '))
 print('Ecco i numeri di Fibonacci minori di', input_number)
