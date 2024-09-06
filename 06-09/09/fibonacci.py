81956adb855ba0fddc72e5988c0697a6207f0770
Aldo Perri <130299266+perri99@users.noreply.github.com>
2024-09-06 11:10:32 +0200

    Add files via upload

---
+++ 06-09/09/fibonacci.py
@@ -0,0 +1,20 @@
+'''
+Fibonacci
+'''
+def fibonacci_number(number): #https://www.valcon.it/python/numeri-di-fibonacci/
+    if number <= 2:
+        return 1
+    else:
+        return fibonacci_number(number-1) + fibonacci_number(number-2)
+
+def find_fibonacci(number):
+    fibonacci_numbers = []
+    for index in range(number):
+        fibonacci = fibonacci_number(index)
+        if fibonacci < number:
+            fibonacci_numbers.append(fibonacci_number(index))
+    return fibonacci_numbers
+
+input_number = int(input('Inserisce un numero: '))
+print('Ecco i numeri di Fibonacci minori di', input_number)
+print(find_fibonacci(input_number))
