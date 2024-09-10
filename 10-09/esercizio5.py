bbb261b95361a2b9e785da82ede4749baca383dd
Aldo Perri <130299266+perri99@users.noreply.github.com>
2024-09-10 16:05:06 +0200

    esercizzio cifrario di CEsare

---
+++ 10-09/esercizio5.py
@@ -0,0 +1,40 @@
+'''
+Scrivete un programma che utilizza il cifrario di Cesare per criptare una
+parola o decriptarla.
+Il Cifrario di Cesare è un algoritmo di crittografia che consiste nello spostare
+ciascuna lettera di una certa quantità di posti nell'alfabeto. Per utilizzarlo, si
+sceglie una chiave (scelta dall’utente) che rappresenta il numero di posti
+di cui ogni lettera dell'alfabeto verrà spostata: ad esempio, se si sceglie
+una chiave di 3, la lettera A diventerà D, la lettera B diventerà E e così via.
+Per decifrare un messaggio cifrato con il cifrario di Cesare bisogna
+conoscere la chiave utilizzata e spostare ogni lettera indietro di un numero
+di posti corrispondente alla chiave.
+'''
+
+def range_char(start='a', stop='z'):
+    return [chr(n) for n in range(ord(start), ord(stop) + 1)]
+
+def encoding(stringa, key):
+    stringa = stringa.lower()
+    substrings = stringa.split(' ')
+    alphabet = range_char()
+    encoded_string = ''
+    for substring in substrings:
+        lista = []
+        for i in substring:
+            if alphabet.index(i) < len(alphabet)-1-key:
+                lista.append(alphabet[alphabet.index(i)+key])
+            else:
+                lista.append(alphabet[key-(len(alphabet)-alphabet.index(i))])
+        substring = ''.join(lista)
+        encoded_string = encoded_string + substring+' '
+    
+    return encoded_string
+
+def decoding(stringa, key):
+    return encoding(stringa, -key)
+
+string_test = "ciao mi chiamo aldo"
+encoded_string = encoding(string_test, 4)
+print(encoded_string)
+print(decoding(encoded_string,4))
