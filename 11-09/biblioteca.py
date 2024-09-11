15ca95054d03f2b379dec0cc97e3cff85937cf7c
Aldo Perri <130299266+perri99@users.noreply.github.com>
2024-09-11 13:00:03 +0200

    mostra_libro in mostra_archivio

--- 11-09/biblioteca.py
+++ 11-09/biblioteca.py
@@ -1,25 +1,36 @@
 import libro
 
 class Biblioteca:
+
     archivio_libri = []
 
-    def crea_libro(titolo, autore, pagine):
+    def crea_libro(self, titolo, autore, pagine):
         Libro = libro.Libro(titolo, autore, pagine)
+        self.archivio_libri.append(Libro)
         return Libro
-    def mostra_libro(Libro):
+    
+    def mostra_libro(self, Libro):
         Libro.descrivi_libro()
+
     def aggiungi_libro(self, Libro):
         self.archivio_libri.append(Libro)
 
     def mostra_archivio(self):
         for Libro in self.archivio_libri:
-            Libro.descrivi_libro()
-
+            #Libro.descrivi_libro()
+            self.mostra_libro(Libro)
 
 Biblioteca1 = Biblioteca()
-Ulisse = Biblioteca.crea_libro('Ulisse', 'Joyce', 4000)
-Zeno = Biblioteca.crea_libro('La coscienza di Zeno', 'Svevo', 400)
-Biblioteca.mostra_libro(Ulisse)
-Biblioteca1.aggiungi_libro(Ulisse)
-Biblioteca1.aggiungi_libro(Zeno)
-Biblioteca1.mostra_archivio()
+while True:
+    mod = int(input('Cosa vuoi fare? 1.Crea Libro 2.Mostra Archivio 3.Esci'))
+    if mod == 1:
+        titolo = input('Inserisci titolo: ')
+        autore = input('inserisci autore: ')
+        pagine = input('Inserisci numero pagine: ')
+        Biblioteca1.crea_libro(titolo, autore, pagine)
+    elif mod == 2:
+        Biblioteca1.mostra_archivio()
+    elif mod == 3:
+        break
+    else:
+        print('Scelta non valida')
