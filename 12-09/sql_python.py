485cc228073741a43eee1a7cad5656aea6bc2560
Aldo Perri <130299266+perri99@users.noreply.github.com>
2024-09-12 20:48:28 +0200

    Update sql_python.py

--- 12-09/sql_python.py
+++ 12-09/sql_python.py
@@ -28,9 +28,18 @@ def inserisci_dati():
     print(mycursor.rowcount, "record inseriti")
     print('Ultimo ID', mycursor.lastrowid)
 
-seleziona = "select * from utenti where id > 2" 
-mycursor.execute(seleziona)
+def selezione():
+    seleziona = "select * from utenti where id > 2" 
+    mycursor.execute(seleziona)
 
-dati = mycursor.fetchall()
-for dato in dati:
-    print(dato)
+    dati = mycursor.fetchall()
+    for dato in dati:
+        print(dato)
+
+def delete():
+    query = 'delete from utenti where id = 2'
+    mycursor.execute(query)
+    mydb.commit()
+    print('Righe eliminate:', mycursor.rowcount)
+
+delete()
