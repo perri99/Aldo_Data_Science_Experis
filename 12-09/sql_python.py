80ced2393f762b5e2624ddee7864cc7c481680ec
Aldo Perri <130299266+perri99@users.noreply.github.com>
2024-09-12 17:59:04 +0200

    Create sql_python.py

---
+++ 12-09/sql_python.py
@@ -0,0 +1,36 @@
+import mysql.connector as sql
+
+mydb = sql.connect(
+    host = 'localhost',
+    user = 'root',
+    password = 'root',
+    port = 3306,
+    database = 'esercitazionepython'
+)
+
+mycursor = mydb.cursor()
+
+#sql1 = "CREATE DATABASE esercitazionepython"
+#mycursor.execute(sql1)
+#mycursor.execute('ALTER TABLE utenti\
+ #                ADD cognome VARCHAR(255)')
+#mycursor.execute("")
+#mycursor.execute("SHOW TABLES")
+def inserisci_dati():
+    inserisci = "INSERT INTO utenti(nome, cognome) VALUES(%s,%s)"
+
+    #values = ('Aldo', 'Perri')
+    values_list = [('Giuseppe','Rossi'), ('Tommaso', 'Verdi')]
+    mycursor.executemany(inserisci, values_list)
+
+    #commit
+    mydb.commit()
+    print(mycursor.rowcount, "record inseriti")
+    print('Ultimo ID', mycursor.lastrowid)
+
+seleziona = "select * from utenti where id > 2" 
+mycursor.execute(seleziona)
+
+dati = mycursor.fetchall()
+for dato in dati:
+    print(dato)
