93e9b2ab86269dd73d6a3dc4e79d9d5caa2658ff
Aldo Perri <130299266+perri99@users.noreply.github.com>
2024-09-16 22:40:02 +0200

    completamento
    Aggiunta media mobile settimanale, perfezionamento dei grafici. Eliminazione ciclo for nella funzione trend_crescente()

--- 16-09/visitatori_giornalieri.py
+++ 16-09/visitatori_giornalieri.py
@@ -9,16 +9,14 @@ def genera_dati(media = 2000, dev_std = 500):
     return data
 
 def trend_crescente(data, incremento = 10): #trend lineare brutale
-    for index in range(len(data)):
-        data[index] += index * incremento #incremento i dati di 10 * numero di giorni passati da inizio anno
+    increments = np.linspace(0,365*incremento, 365)
+    data = np.add(data, increments) #incremento i dati di incremento * numero di giorni passati da inizio anno
     return data
 
 def serie_temporale(data):
     temporal_range = pd.date_range(start='2023-01-01', end = '2023-12-31')
     dizionario = {'Data':temporal_range,
-                  'Visitatori':[]}
-    for index in range(len(data)):
-        dizionario['Visitatori'].append(data[index])
+                  'Visitatori':data}
     df = pd.DataFrame(dizionario)
     df.set_index('Data', inplace=True)
     return df
@@ -31,13 +29,25 @@ def medie_mensili(dataframe):
 visitatori_giornalieri = genera_dati()
 serie = serie_temporale(visitatori_giornalieri)
 print(serie.describe())
-visitatori_giornalieri = trend_crescente(visitatori_giornalieri)
+visitatori_giornalieri = trend_crescente(visitatori_giornalieri, incremento = 1)
 serie = serie_temporale(visitatori_giornalieri)
+medie, deviazioni = medie_mensili(serie)
 print(serie.describe())
+
+# Calcolo della media mobile settimanale
+serie['Media Mobile Settimanale'] = serie['Visitatori'].rolling(window=7).mean()
+print(serie.head(14))
+#-------Plotting data---------------------
 months = ['Jan','Feb', 'Mar', 'Apr', 'May', 'June', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 days = np.arange(0,365)
-medie, deviazioni = medie_mensili(serie)
-plt.plot(months, medie)
+# Grafico delle medie mensili
+plt.scatter(months, medie, marker='*', linewidths=0.5)
+plt.title('Medie mensili')
 plt.show()
-plt.plot(days, visitatori_giornalieri)
+
+# Grafico visitatori giornalieri con media mobile settimanale
+plt.plot(days, visitatori_giornalieri, label='Visitatori Giornalieri', alpha=0.5)
+plt.plot(days, serie['Media Mobile Settimanale'], label='Media Mobile Settimanale (7 giorni)', color='red', linewidth=2)
+plt.title('Visitatori Giornalieri con Media Mobile Settimanale')
+plt.legend()
 plt.show()
