ec484b8ffb2dbdb743b018dad15b3e15c3223853
Aldo Perri <130299266+perri99@users.noreply.github.com>
2024-09-17 12:28:20 +0200

    esercizio vendite

---
+++ 17-09/vendite.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import numpy as np
+
+anno = 2024
+mese = 3
+
+cities = ['Roma', 'Bologna', 'Milano']
+products = ['Chitarra', 'Basso', 'Batteria']
+dates = pd.date_range(start=f'{anno}-{mese:02d}-01', end=f'{anno}-{mese:02d}-30')
+
+vendite = np.random.randint(50,100,30)
+data_cities = [np.random.choice(cities) for _ in range(30)]
+data_prducts= [np.random.choice(products) for _ in range(30)]
+
+df = pd.DataFrame({'Data': dates,
+                  'Città': data_cities,
+                  'Prodotto': data_prducts,
+                  'Vendite': vendite})
+
+print('dataframe originale')
+print(df)
+#creazione tabella pivot mostrante la media 
+pivot_df = df.pivot_table(values='Vendite', index='Prodotto', columns='Città', aggfunc='mean')
+print('Tabella Pivot')
+print(pivot_df)
+#metodo groupby
+df_senzadata = df.drop(columns = 'Data')
+grouped_df  = df_senzadata.groupby('Prodotto').sum()
+print('Risultato di groupby:')
+print(grouped_df)
