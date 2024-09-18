c18a1794b9fc89e2f2ed1f02cb4a81940a6689dc
Aldo Perri <130299266+perri99@users.noreply.github.com>
2024-09-18 11:10:29 +0200

    inserimento scatterPlot

--- 18_09/visualizzazione/esercizio1.py
+++ 18_09/visualizzazione/esercizio1.py
@@ -5,6 +5,7 @@ import seaborn as sns
 
 temperatures = np.random.randint(20, 30, 30)
 df = pd.DataFrame({'Temperatures': temperatures})
+print(df.describe())
 
 temperatura_massima = [df['Temperatures'].max()]*30
 temperatura_minima = [df['Temperatures'].min()]*30
@@ -13,14 +14,23 @@ temperatura_mediana = [df['Temperatures'].median()]*30
 
 days = np.arange(1,31)
 
-plt.bar(days, df['Temperatures'], label = 'Temperatura Giornaliera')
-plt.plot(days, temperatura_massima, label = 'Temperatura massima')
-plt.plot(days, temperatura_minima, label = 'Temperatura minima')
-plt.plot(days, temperatura_media, label = 'Temperatura media')
-plt.plot(days, temperatura_mediana, label = 'Temperatura mediana')
+plt.bar(days, df['Temperatures'])
+plt.plot(days, temperatura_massima, label='Temperatura massima', color='red')
+plt.plot(days, temperatura_minima, label='Temperatura minima', color='yellow')
+plt.plot(days, temperatura_media, label='Temperatura media', color='green', linestyle='--')
+plt.plot(days, temperatura_mediana, label='Temperatura mediana', color='orange', linestyle='--')
 plt.legend()
 plt.title('Temperatura mensile')
-plt.ylim(18,30)
+plt.ylim(19,30)
 plt.xlabel('Giorni')
 plt.ylabel('Temperatura °C')
 plt.show()
+plt.scatter(days, df['Temperatures'], marker = '*')
+plt.plot(days, temperatura_media, label='Temperatura media', color='green', linestyle='-')
+plt.plot(days, temperatura_mediana, label='Temperatura mediana', color='orange', linestyle='-')
+plt.title('Temperatura Mensile')
+plt.xlabel('Giorni')
+plt.ylabel('Temperatura °C')
+plt.legend()
+plt.show()
+
