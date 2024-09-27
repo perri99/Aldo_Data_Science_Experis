165d8edb9b29e89b24340989cd080563eaac1ca5
Aldo Perri <130299266+perri99@users.noreply.github.com>
2024-09-27 10:32:21 +0200

    Update chroma.py

--- 26-09/chroma.py
+++ 26-09/chroma.py
@@ -1,5 +1,16 @@
 import chromadb
+import asyncio
 
 client = chromadb.PersistentClient(path = '/savechroma')
 
-print(client.heartbeat()/(10**9 * 60 *60 *24)) #controlla connettività server chroma)
+print(client.heartbeat()) #controlla connettività server chroma)
+
+client.reset() #resetta database
+
+# chroma_client = chromadb.HttpClient(host = 'localhost', port = 8000) #port = 8000 oppure 8080
+#async preposizione funzionale
+async def main():
+    client = await chromadb.AsyncHttpClient()
+    collection = await client.create_collection(name = 'my_collection')
+    await collection.add(documents = ['hello world'], ids = ['id1'])
+    
