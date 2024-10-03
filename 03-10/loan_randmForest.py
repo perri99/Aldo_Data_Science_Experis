0c01561bc31fe1469f24fba205d99d08bfb873f3
Aldo Perri <130299266+perri99@users.noreply.github.com>
2024-10-03 15:25:16 +0200

    Update loan_randmForest.py

--- 03-10/loan_randmForest.py
+++ 03-10/loan_randmForest.py
@@ -28,32 +28,33 @@ test_values = scaler.transform(df_test)
 X_train, X_test, y_train, y_test = train_test_split(values, loan_status, test_size=0.2, random_state=42)
 
 # Definizione del modello RandomForestClassifier
-model = RandomForestClassifier(n_estimators=250,
-                               min_samples_split=10,
-                               min_samples_leaf=2,
-                               bootstrap=False)
-
+# model = RandomForestClassifier(n_estimators=250,
+                            #    min_samples_split=10,
+                            #    min_samples_leaf=2,
+                            #    bootstrap=False)
+model = RandomForestClassifier()
 # Definizione dello spazio di ricerca degli iperparametri
-# param_dist = {
-#     'n_estimators': np.arange(50, 500, 50),            # Numero di alberi nella foresta
-#     'max_depth': [None, 10, 20, 30, 40, 50],           # Profondità massima di ogni albero
-#     'min_samples_split': [2, 5, 10],                   # Numero minimo di campioni per suddividere un nodo
-#     'min_samples_leaf': [1, 2, 4],                     # Numero minimo di campioni in un nodo foglia
-#     'bootstrap': [True, False]                         # Utilizzo del bootstrapping
-# }
+param_dist = {
+    'criterion': ['gini', 'entropy', 'log_loss'],
+    'n_estimators': np.arange(50, 500, 50),            # Numero di alberi nella foresta
+    'max_depth': [None, 10, 20, 30, 40, 50],           # Profondità massima di ogni albero
+    'min_samples_split': [2, 5, 10],                   # Numero minimo di campioni per suddividere un nodo
+    'min_samples_leaf': [1, 2, 4],                     # Numero minimo di campioni in un nodo foglia
+    'bootstrap': [True, False]                         # Utilizzo del bootstrapping
+}
 
-# # RandomizedSearchCV con 10 iterazioni
-# random_search = RandomizedSearchCV(estimator=model, param_distributions=param_dist,
-#                                    n_iter=10, cv=5, verbose=1, random_state=42, n_jobs=-1)
+# RandomizedSearchCV con 10 iterazioni
+random_search = RandomizedSearchCV(estimator=model, param_distributions=param_dist,
+                                   n_iter=10, cv=5, verbose=1, random_state=42, n_jobs=-1)
 
-# Fitting
-# random_search.fit(X_train, y_train)
+#Fitting
+random_search.fit(X_train, y_train)
 
-# # Migliori iperparametri trovati
-# print(f'Best parameters found: {random_search.best_params_}')
+# Migliori iperparametri trovati
+print(f'Best parameters found: {random_search.best_params_}')
 
-# # Predizione e valutazione sul test set
-# best_model = random_search.best_estimator_
+# Predizione e valutazione sul test set
+model = random_search.best_estimator_
 model.fit(X_train, y_train)
 predictions = model.predict(X_test)
 performances = classification_report(y_test, predictions)
